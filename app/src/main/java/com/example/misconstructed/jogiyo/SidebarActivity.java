131f276487ed15efea04fd2123509f530d25f932
junbock <33506716+junbock@users.noreply.github.com>
2017-11-26 20:15:46 +0900

    o

--- app/src/main/java/com/example/misconstructed/jogiyo/SidebarActivity.java
+++ app/src/main/java/com/example/misconstructed/jogiyo/SidebarActivity.java
@@ -11,26 +11,31 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class SidebarActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     private ConstraintLayout mymap;
-    private ConstraintLayout checkin;
+    private RelativeLayout checkin;
     private RelativeLayout preferences;
+    private TextView label;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sidebar);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-
-        toolbar.setTitle("My Map");
-        //toolbar.setElevation();
-        //toolbar.set
-
+        ListView listView = (ListView) findViewById(R.id.listView);
+        toolbar.setTitle("");
         setSupportActionBar(toolbar);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -43,11 +48,25 @@ public class SidebarActivity extends AppCompatActivity
         navigationView.setNavigationItemSelectedListener(this);
 
         mymap = (ConstraintLayout)findViewById(R.id.mymap);
-        checkin = (ConstraintLayout)findViewById(R.id.checkin);
+        checkin = (RelativeLayout)findViewById(R.id.checkin);
+
+        final ListItemAdapter adapter = new ListItemAdapter();
+        adapter.addItem(new ListItem("해파리 낚시","12시 00분",true,false));
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                ListItem item = (ListItem) adapter.getItem(position);
+                Toast.makeText(getApplicationContext(), "선택 : " + item.getName(),
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+
         preferences = (RelativeLayout) findViewById(R.id.preferences);
         mymap.setVisibility(View.VISIBLE);
         checkin.setVisibility(View.INVISIBLE);
         preferences.setVisibility(View.INVISIBLE);
+        label=(TextView)findViewById(R.id.title);
     }
 
     @Override
@@ -67,7 +86,7 @@ public class SidebarActivity extends AppCompatActivity
         int id = item.getItemId();
 
         mymap = (ConstraintLayout)findViewById(R.id.mymap);
-        checkin = (ConstraintLayout)findViewById(R.id.checkin);
+        checkin = (RelativeLayout)findViewById(R.id.checkin);
         preferences = (RelativeLayout) findViewById(R.id.preferences);
 
         Intent intent = null;
@@ -77,25 +96,68 @@ public class SidebarActivity extends AppCompatActivity
             mymap.setVisibility(View.VISIBLE);
             checkin.setVisibility(View.INVISIBLE);
             preferences.setVisibility(View.INVISIBLE);
+            label.setText("My Map");
         } else if (id == R.id.checkin_menu) {
             Toast.makeText(getApplicationContext(), "Check In", Toast.LENGTH_LONG).show();
             mymap.setVisibility(View.INVISIBLE);
             checkin.setVisibility(View.VISIBLE);
             preferences.setVisibility(View.INVISIBLE);
+            label.setText("Check In");
         } else if (id == R.id.preferences_menu) {
             Toast.makeText(getApplicationContext(), "Preferences", Toast.LENGTH_LONG).show();
             mymap.setVisibility(View.INVISIBLE);
             checkin.setVisibility(View.INVISIBLE);
             preferences.setVisibility(View.VISIBLE);
+            label.setText("Preferences");
         } else if (id == R.id.logout_menu) {
             Toast.makeText(getApplicationContext(), "Log Out", Toast.LENGTH_LONG).show();
             intent = new Intent(this, LoginActivity.class);
             startActivity(intent);
         }
 
+
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
 
         return true;
+    }
+
+    private class ListItemAdapter extends BaseAdapter{
+
+        private ArrayList<ListItem> items = new ArrayList();
+
+        @Override
+        public int getCount() {
+            return items.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return items.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup viewGroup) {
+            ListItemView listView = (ListItemView) convertView;
+            if (convertView == null)
+                listView = new ListItemView(getApplicationContext());
+
+            ListItem item = items.get(position);
+
+            listView.setName(item.getName());
+            listView.setPlace();
+            listView.setTime(item.getTime());
+            listView.setStar(item.isStar());
+            listView.setCheck(item.isCheck());
+
+            return listView;
+        }
+
+        void addItem(ListItem item) { items.add(item); }
     }
 }
